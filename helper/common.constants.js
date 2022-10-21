192613aff431e22de8c051554fb2de724c849bf6
Nicholas Roberts <nicholas@browserstack.com>
2022-10-20 21:16:43 -0700

    removed single test file for cicd

--- helper/common.constants.js
+++ helper/common.constants.js
@@ -1,8 +1,4 @@
 module.exports = Object.freeze({
   PORT: '443',
-  TEST_WIDTHS: [375, 480, 720, 1280, 1440, 1920],
-  BASELINE_URL: 'https://www.browserstack.com', // is production url in common constants
-  PERCY_BASESLINE_URL_NAME: 'Browserstack.coom Production',
-  PRODUCTION_URL: 'https://www.browserstack.com',
-  STAGING_URL: 'https://k8s.bsstag.com'
+  TEST_WIDTHS: [375, 480, 720, 1280, 1440, 1920]
 });
--- helper/staging.constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.freeze({
-  PORT: '443',
-  TEST_WIDTHS: [375, 480, 720, 1280, 1440, 1920],
-  PERCY_BASE_URL_NAME: 'Staging',
-  PERCY_BRANCH: 'staging',
-  PERCY_TARGET_BRANCH: 'master',
-  PRODUCTION_URL: 'https://www.browserstack.com',
-  STAGING_URL: 'https://k8s.bsstag.com'
-});
